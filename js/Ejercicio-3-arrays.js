351594b509013835f7f980f694c8c0d4a4100eaa
rasalgh0ul <arkantos983@gmail.com>
2022-07-21 19:05:41 -0300

    agregado el ejercicio de dados

--- js/Ejercicio-3-arrays.js
+++ js/Ejercicio-3-arrays.js
@@ -1,20 +1,42 @@
-let suma = [];
 
-function tirarDados(){
-    let numero1 = Math.floor(Math.random() * 6)+1;
-    let numero2 = Math.floor(Math.random() * 6)+1;
-    return numero1 + numero2;
-}
+let dado1 = Math.floor(Math.random()*(6 - 1 +1)+1);
+let dado2 = Math.floor(Math.random()*(6 - 1 +1)+1);
 
-////////////////////////////////////////
+let sumas = [];
+let pruebaDado1 = [];
+let pruebaDado2 = [];
 
 for (let i = 0; i < 50; i++) {
-    suma.push(tirarDados())
-     
+    
+    let dado1 = Math.floor(Math.random()*(6 - 1 +1)+1);
+    let dado2 = Math.floor(Math.random()*(6 - 1 +1)+1);
+    let suma = dado1 + dado2
+    sumas.push(suma);
+    pruebaDado1.push(dado1)
+    pruebaDado2.push(dado2)
 }
 
+document.write(`resultado del 1er dado ${pruebaDado1} <br>`)
+document.write(`resultado del 2do dado ${pruebaDado2}<br>`)
+document.write(`resultado de las sumas ${sumas}`)
 
-document.write(suma)
+document.write(`<table>
+<tbody>
+<tr><td>Resultado</td><td>Frecuencia</td> </tr>
+`)
+
+for (let resultado = 2; resultado <= 50; resultado++){
+    let contador = 0;
+    for(let posicion = 0; posicion < sumas.length; posicion++){
+        if (resultado === sumas[posicion]){
+            //sumar las apariciones
+            contador++
+        }
+    }
+    document.write(`<tr><td>${resultado}</td><td>${contador}</td></tr>`)
+    
+}
+document.write(`</tbody></table>`)
 
 
 
--- js/Ejercicio-3-funciones.js
+++ js/Ejercicio-3-funciones.js
@@ -1,10 +1,15 @@
 let numero1 = parseInt(prompt("Ingrese el valor de un lado del rectangulo"))
 let numero2 = parseInt(prompt("Ingrese el valor del segundo lado del rectangulo"))
+console.log(numero1)
+console.log(numero2)
+
 
 function perimetro(){
     p = 2*(numero1 + numero2)
-    return p;
 }
 
 perimetro()
 document.write(`El perimetro es ${p}`)
+
+
+
